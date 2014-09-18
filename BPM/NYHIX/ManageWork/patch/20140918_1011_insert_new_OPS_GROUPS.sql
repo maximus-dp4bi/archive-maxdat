de5b10c054b5f7f4c664822fffb00b06fd97f117
pa28085 <pa28085@maximus.com>
2014-09-18 16:29:44 +0000

    Removed an insert as it already exists in the table.
    git-svn-id: svn://svn-staging.maximus.com/dev1d/maxdat@12011 3a208796-2f9a-46b6-afea-cdc47f3b9e6b

--- BPM/NYHIX/ManageWork/patch/20140918_1011_insert_new_OPS_GROUPS.sql
+++ BPM/NYHIX/ManageWork/patch/20140918_1011_insert_new_OPS_GROUPS.sql
@@ -3,7 +3,6 @@ Created on 18-Sep-2014 by Raj A. for NYHIX-10852
 */
 insert into BPM_D_OPS_GROUP_TASK  values ('Research','DERT-Other');
 
-insert into BPM_D_OPS_GROUP_TASK  values ('Research','DPR - Application-Missing Data');  
 insert into CORP_ETL_LIST_LKUP (cell_id,name,list_type, value, out_var, ref_type, ref_id, start_date, end_date, comments, created_ts, updated_ts) 
 VALUES (seq_cell_id.nextval,'ManageWork_SLA_Days_Type','TASK_TYPE','DPR - Application-Missing Data','C','STEP_DEFINITION_ID',2013058,trunc(sysdate-1),to_date('07077777','mmddyyyy'),'Initial Load',sysdate,sysdate);	
 insert into CORP_ETL_LIST_LKUP (cell_id,name,list_type, value, out_var, ref_type, ref_id, start_date, end_date, comments, created_ts, updated_ts) 
