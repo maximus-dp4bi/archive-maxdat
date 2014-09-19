794c4f89efd8aa961763c04b18c07a3892eab490
sk51922 <sk51922@maximus.com>
2014-09-19 19:53:53 +0000

    patch to insert new uow ids
    git-svn-id: svn://svn-staging.maximus.com/dev1d/maxdat@12049 3a208796-2f9a-46b6-afea-cdc47f3b9e6b

---
+++ BPM/Corp/ProductionPlanning/createdb/insert_to_PP_CFG_UNIT_OF_WORK.sql
@@ -0,0 +1,5 @@
+insert into pp_cfg_unit_of_work values(18,'NYHIX_VERIF','N','Minutes',3,'BUS',5,'VERIF');
+
+insert into pp_cfg_unit_of_work values(19,'NYHIX_RESEARCH','N','Minutes',2,'BUS',10,'RESEARCH');
+
+commit;
