b97b2ccb485a116adca36940afc926c8def127c8
sp57859 <sp57859@maximus.com>
2014-09-23 18:34:21 +0000

    updated file
    git-svn-id: svn://svn-staging.maximus.com/dev1d/maxdat@12093 3a208796-2f9a-46b6-afea-cdc47f3b9e6b

--- BPM/NYHIX/MailFaxDocV2/createdb/cleanup_staging_tables.sql
+++ BPM/NYHIX/MailFaxDocV2/createdb/cleanup_staging_tables.sql
@@ -1,6 +1,8 @@
 truncate table NYHIX_ETL_MAIL_FAX_DOC_V2;
 truncate table NYHIX_ETL_MAIL_FAX_DOC_OLTP_V2;
 truncate table NYHIX_ETL_MAIL_FAX_DOC_WIP_V2;
+truncate table NYHIX_ETL_MAIL_FAX_DOC_CSC_V2;
+truncate table NYHIX_ETL_MAIL_FAX_DOC_APP_V2;
 
 truncate table DOCUMENT_STG;
 truncate table APP_DOC_DATA_STG;
