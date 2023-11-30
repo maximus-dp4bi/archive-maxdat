9afaeddddefd63988d76c9259073e56ff97ea8b4
Guy Thibodeau <guydthibodeau@maximus.com>
2023-11-30 10:18:20 -0800

    Create 20231129_1534_Rollback_Task_Type_SLA.sql

---
+++ BPM/NYHIX/MW_V2/patch/20231129_1534_Rollback_Task_Type_SLA.sql
@@ -0,0 +1,14 @@
+-- Rollback D_TASK_TYPES IAW FEATURE 36456
+update maxdat.d_task_types set sla_jeopardy_days = null, sla_days = null where task_name = 'Application Rejection';
+update maxdat.d_task_types set sla_jeopardy_days = null, sla_days = 2 where task_name = 'DPR - Nav/CAC ID Proofing';
+update maxdat.d_task_types set sla_jeopardy_days = 2, sla_days = 3 where task_name = 'Multiple Applications';
+update maxdat.d_task_types set sla_jeopardy_days = 14, sla_days = 20 where task_name = 'DPR - Application in Process';
+update maxdat.d_task_types set sla_jeopardy_days = null, sla_days = null where task_name = 'DPR - Existing Appeal';
+update maxdat.d_task_types set sla_jeopardy_days = 1, sla_days = 0 where task_name = 'Relink Request';
+update maxdat.d_task_types set sla_jeopardy_days = 2, sla_days = 3 where task_name = 'Authorized Representative Task';
+update maxdat.d_task_types set sla_jeopardy_days = 2, sla_days = 3 where task_name = 'Authorized Representative- Upload';
+update maxdat.d_task_types set sla_jeopardy_days = 7, sla_days = 10 where task_name = 'Returned Mail';
+update maxdat.d_task_types set sla_jeopardy_days = 1, sla_days = 2 where task_name = 'Large Font Notice Request';
+update maxdat.d_task_types set sla_jeopardy_days = 21, sla_days = 25 where task_name = 'DPR - Application-Missing Data';
+update maxdat.d_task_types set sla_jeopardy_days = null, sla_days = null where task_name = 'Application Assessment';
+commit;
